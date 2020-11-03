d8a8c02d9d7b9dbcc0812acf67a21f8784d345ce
Joseph-112 <44301921+Joseph-112@users.noreply.github.com>
2020-11-03 14:38:44 -0500

    Documentación

---
+++ src/main/java/controller/Controlador1.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controller;
+
+import javax.inject.Named;
+import javax.enterprise.context.Dependent;
+
+/**
+ *
+ * @author Joseph
+ */
+@Named(value = "controlador1")
+@Dependent
+public class Controlador1 {
+
+    /**
+     * Creates a new instance of Controlador1
+     */
+    public Controlador1() {
+    }
+    
+}
