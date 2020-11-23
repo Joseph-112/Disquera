1d91b03aae03940a7834216c455fa821662a1cac
Joseph-112 <44301921+Joseph-112@users.noreply.github.com>
2020-11-23 01:00:19 -0500

    Avances menú artista

--- src/main/java/controller/crearartistaController.java
+++ src/main/java/controller/crearartistaController.java
@@ -6,10 +6,18 @@
 package controller;
 
 import java.io.IOException;
+import java.io.Serializable;
 import java.sql.Date;
+import java.util.ArrayList;
+import java.util.List;
 import javax.inject.Named;
 import javax.enterprise.context.RequestScoped;
 import javax.faces.context.FacesContext;
+import javax.faces.model.SelectItem;
+import model.DAOGenre;
+import model.DAONationality;
+import pojo.Genre;
+import pojo.Nationality;
 
 /**
  *
@@ -17,36 +25,49 @@ import javax.faces.context.FacesContext;
  */
 @Named(value = "crearartistaController")
 @RequestScoped
-public class crearartistaController {
+public class crearartistaController implements Serializable{
+    
+    private static List<SelectItem> genreList;
+    private static List<SelectItem> countryList;
+    private Nationality country;
+    private Genre musicGenre;
     private String nombreartista;
-    private String nacionalidad;
     private Date fechanacimiento;
     private String fotoartista;
+    private String nacionalidad;
     
     /**
      * Creates a new instance of crearartistaController
      */
     public crearartistaController() {
-        //no se que va acá :(
+        country = new Nationality();
+        musicGenre = new Genre();
     }
 
     public void enviardatos(){//recpcion y muestra de datos desde el boton por consola
         System.out.println("Entro"+ nombreartista+" "+ nacionalidad+" "+fechanacimiento+" "+fotoartista);
     }
-    public String getUsername() {
-        return nombreartista;
+    
+    public List<SelectItem> countryList(){
+        countryList = new ArrayList<SelectItem>();
+        List<Nationality> countries;
+        countries = new DAONationality().nationalityList();
+        for (Nationality country : countries){
+            SelectItem countryItem = new SelectItem (country.getId_nationality(),country.getCountry());
+            countryList.add(countryItem);
+        }
+        return countryList;
     }
-
-    public void setUsername(String username) {
-        this.nombreartista = username;
-    }
-
-    public String getNacionalidad() {
-        return nacionalidad;
-    }
-
-    public void setNacionalidad(String nacionalidad) {
-        this.nacionalidad = nacionalidad;
+    
+    public List<SelectItem> genreList(){
+        genreList = new ArrayList<SelectItem>();
+        List<Genre> genres;
+        genres = new DAOGenre().genreList();
+        for (Genre genre : genres){
+            SelectItem countryItem = new SelectItem (genre.getId_genre(),genre.getGenre());
+            genreList.add(countryItem);
+        }
+        return genreList;
     }
 
     public String getNombreartista() {
@@ -72,5 +93,46 @@ public class crearartistaController {
     public void setFotoartista(String fotoartista) {
         this.fotoartista = fotoartista;
     }
+
+    public List<SelectItem> getGenreList() {
+        return genreList;
+    }
+
+    public void setGenreList(List<SelectItem> genreList) {
+        this.genreList = genreList;
+    }
+
+    public List<SelectItem> getCountryList() {
+        return countryList;
+    }
+
+    public void setCountryList(List<SelectItem> countryList) {
+        this.countryList = countryList;
+    }
+
+    public Nationality getCountry() {
+        return country;
+    }
+
+    public void setCountry(Nationality country) {
+        this.country = country;
+    }
+
+    public String getNacionalidad() {
+        return nacionalidad;
+    }
+
+    public void setNacionalidad(String nacionalidad) {
+        this.nacionalidad = nacionalidad;
+    }
+
+    public Genre getMusicGenre() {
+        return musicGenre;
+    }
+
+    public void setMusicGenre(Genre musicGenre) {
+        this.musicGenre = musicGenre;
+    }
+    
     
 }
